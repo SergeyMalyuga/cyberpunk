38c5314516ef11d565e2b10938194ff5299eab6e
Sergey Malyuga <sergey@malyuga.ru>
2025-11-05 16:21:50 +0300

    Создать stock.component.

---
+++ src/app/features/stock/stock.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock',
+  imports: [],
+  templateUrl: './stock.component.html',
+  styleUrl: './stock.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StockComponent {
+
+}
