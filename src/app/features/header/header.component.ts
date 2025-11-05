a4f1870a35a862166fbb73908e435a2675da1585
Sergey Malyuga <sergey@malyuga.ru>
2025-11-05 12:59:20 +0300

    Создать header.component. Завершить вёрстку.

---
+++ src/app/features/header/header.component.ts
@@ -0,0 +1,16 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {AppRoute} from '../../core/constants/const';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderComponent {
+  public readonly AppRoute = AppRoute;
+}
