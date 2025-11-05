b83edaff7804019b86867d7d8c6b993101ce9939
Sergey Malyuga <sergey@malyuga.ru>
2025-11-05 16:21:50 +0300

    Создать stock.component.

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from '../../features/header/header.component';
 import { HeroComponent } from '../../features/hero/hero.component';
 import {AboutComponent} from '../../features/about/about.component';
+import {StockComponent} from '../../features/stock/stock.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, HeroComponent, AboutComponent],
+  imports: [HeaderComponent, HeroComponent, AboutComponent, StockComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
