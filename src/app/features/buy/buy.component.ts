f54da2e4fa36a4ef6ebf58c9e38fe20f4f46b23a
Sergey Malyuga <sergey@malyuga.ru>
2025-11-06 21:17:43 +0300

    Создать buy.component

---
+++ src/app/features/buy/buy.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buy',
+  imports: [],
+  templateUrl: './buy.component.html',
+  styleUrl: './buy.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BuyComponent {
+
+}
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -4,10 +4,18 @@ import {HeroComponent} from '../../features/hero/hero.component';
 import {AboutComponent} from '../../features/about/about.component';
 import {StockComponent} from '../../features/stock/stock.component';
 import {HpPromoComponent} from '../../features/hp-promo/hp-promo.component';
+import { BuyComponent } from '../../features/buy/buy.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, HeroComponent, AboutComponent, StockComponent, HpPromoComponent],
+  imports: [
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    StockComponent,
+    HpPromoComponent,
+    BuyComponent,
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
